330e06c22b43f3c78048d0b64c04382338274dd5
Brian de Alwis <bsd@mt.ca>
2017-03-11 00:50:39 -0500

    Fix "Classes implementing method" to report class

--- ca.ubc.cs.ferret.jdt/src/ca/ubc/cs/ferret/jdt/ClassesImplementingInterfaceMethod.java
+++ ca.ubc.cs.ferret.jdt/src/ca/ubc/cs/ferret/jdt/ClassesImplementingInterfaceMethod.java
@@ -4,14 +4,13 @@
  */
 package ca.ubc.cs.ferret.jdt;
 
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.jdt.core.IMethod;
-
-import ca.ubc.cs.clustering.StupidlySimpleRelation;
 import ca.ubc.cs.ferret.model.IRelation;
+import ca.ubc.cs.ferret.model.NamedJoinRelation;
 import ca.ubc.cs.ferret.model.ObjectOrientedRelations;
 import ca.ubc.cs.ferret.model.SimpleSolution;
 import ca.ubc.cs.ferret.types.FerretObject;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.core.IMethod;
 
 /**
  * @author bsd
@@ -26,8 +25,8 @@ public class ClassesImplementingInterfaceMethod extends
 
     protected void internalRun(IProgressMonitor monitor) {
         monitor.beginTask(getDescription(), 20);
-        IRelation impls = getSphere().resolve(monitor,
-        		ObjectOrientedRelations.OP_IMPLEMENTORS, parameter);  
+		IRelation impls = new NamedJoinRelation(ObjectOrientedRelations.OP_IMPLEMENTORS,
+				ObjectOrientedRelations.OP_DECLARING_TYPE).resolve(monitor, getSphere(), parameter);
         for(FerretObject m : impls) {
         	SimpleSolution s = new SimpleSolution(this, this);
         	s.add("implementation", m);
