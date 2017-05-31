7afb8f34022dc6a708559de0ad6ab3654ce5e9cf
Brian de Alwis <bsd@mt.ca>
2017-05-31 11:01:01 -0400

    Do not reissue queries on breakpoint change

--- ca.ubc.cs.ferret/src/ca/ubc/cs/ferret/ModificationVisitor.java
+++ ca.ubc.cs.ferret/src/ca/ubc/cs/ferret/ModificationVisitor.java
@@ -18,23 +18,19 @@ import org.eclipse.core.runtime.CoreException;
  * Walk a resource delta to see if there was an actual change in content or
  * movement.  Ignore changes to markers (MARKER), description (DESCRIPTION),
  * and synchronization information (SYNC).
- * @author bsd
  */
 public class ModificationVisitor implements IResourceDeltaVisitor {
     protected boolean wasModified = false;
     
-    public ModificationVisitor() {
-    }
-
-    protected static int ignoredFlags = IResourceDelta.MARKERS
-            & IResourceDelta.DESCRIPTION & IResourceDelta.SYNC;
+	protected static final int ignoredFlags = IResourceDelta.MARKERS
+			| IResourceDelta.DESCRIPTION | IResourceDelta.SYNC;
 
     public boolean visit(IResourceDelta delta) throws CoreException {
         int eventFlags = delta.getFlags();
         if((eventFlags & ~ignoredFlags) != 0) {
             wasModified = true;
         }
-//      if we've determined is was a modification, then don't bother continuing
+		// if we've determined is was a modification, then don't bother continuing
         return !wasModified;    
     }
 
